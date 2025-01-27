2f4542820d165e91bb128c31270c925b4a31b978
ad0rshcodes <adarsharma.me@gmail.com>
2025-01-26 23:19:12 -0600

    Added resources

--- projects/Classic-Encryption/Encryption.java
+++ projects/Classic-Encryption/Encryption.java
@@ -6,17 +6,26 @@
  * sharmaad@grinnell.edu
  * 
  * Acknowledgements:
+ * 1. java 21.0.1 2023-10-17 LTS
+ * 2. VS Code 1.96.4
+ * 3. TTAP: Data Structures - Lab Manual
+ * 4. W3schools: https://www.w3schools.com/java/java_ref_string.asp
+ * 5. Prof. Peter Osera
  */
 
 public class Encryption {
+
+    // custom error messages for invalid input
     public static void main(String[] args) {
 
+        // if the number of arguments are less than or greater than 2
         if (args.length != 2) {
             System.out.println("Usage: java CaeserCipher <encode|decode> <text>");
             System.out.println();
             return;
         }
 
+        // error if there are any non-lowercase english characters
         for (int i = 0; i < args[1].length(); i++) {
             // System.out.println(args[1].charAt(i));
             if (args[1].charAt(i) < 97 || args[1].charAt(i) > 122) {
@@ -35,6 +44,7 @@ public class Encryption {
         }
     }
 
+    // printing the output for both encoding and decoding
     public static void printCodedStrings(String choice, String inputString) {
         if (choice.equals("encode")) {
             for (int i = 0; i <= 25; i++) {
@@ -47,6 +57,7 @@ public class Encryption {
         }
     }
 
+    // fn to encode a given string by value n
     public static String encode(int n, String s) {
         s = s.toLowerCase();
         int length = s.length();
@@ -63,6 +74,7 @@ public class Encryption {
         return encodedString;
     }
 
+    // fn to decode a given string by value n
     public static String decode(int n, String s) {
 
         s = s.toLowerCase();
